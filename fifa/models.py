c89bf935988e68ae439d70915cd56be97cdd5069
Orlando Hernandez <ohernandezn@unal.edu.co>
2021-06-12 12:42:08 -0500

    Create Team and player models

--- fifa/models.py
+++ fifa/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Team(models.Model):
+    """
+        Team model class
+    """
+    name = models.CharField(max_length=128, unique=True)
+
+
+class Player(models.Model):
+    """
+        Player model class
+    """
+    name = models.CharField(max_length=64)
+    position = models.CharField(max_length=32)
+    nation = models.CharField(max_length=64)
+
+    team = models.ForeignKey(
+        Team,
+        related_name='players',
+        on_delete=models.deletion.CASCADE
+    )
